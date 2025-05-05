91d36512f223ee8d49a87af5e6b741054a2bba2a
Ylnarasimha1711 <rockynarasimha17@gmail.com>
2025-05-06 02:04:13 +0530

    CI CID deploy vercel

--- Backend/server.js
+++ Backend/server.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 9002;
 const secret = 'thisissecret'
 const path = require('path');
 const cron = require('node-cron')
-const csrf = require("csurf"); // Ensure this is included
+const csrf = require("csurf"); // Ensure this is as included
 
 
 // var instance = new Razorpay({
