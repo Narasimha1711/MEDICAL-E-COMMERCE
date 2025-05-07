ef15f78065523bfaadbc070829b1e69bf3f026c3
Narasimha1711 <rockynarasimha17@gmail.com>
2025-05-07 15:19:05 +0530

    made some changes mui/icons

--- Frontend/src/pages/AboutUs.jsx
+++ Frontend/src/pages/AboutUs.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import AboutHero from '../components/About/AboutHero';
-import TeamSection from '../components/About/TeamSection';
-import Features from '../components/About/Features';
-import FAQ from '../components/About/FAQ';
-import Contact from '../components/About/Contact';
+import AboutHero from '../Components/About/AboutHero';
+import TeamSection from '../Components/About/TeamSection';
+import Features from '../Components/About/Features';
+import FAQ from '../Components/About/FAQ';
+import Contact from '../Components/About/Contact';
 import './aboutUs.css'
 // import './aboutUs2.css'
 
